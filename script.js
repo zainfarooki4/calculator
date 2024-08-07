247202141dd4e473223121cc3f8846ee3e101d56
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2024-08-07 17:33:36 -0500

    Finished basic function of calculator and defined global variables.

---
+++ script.js
@@ -0,0 +1,38 @@
+// Define the variables (global)
+    let firstNumber;
+    let operator;
+    let secondNumber;
+// Create functions for add/subtract/multiply/divide
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+// Create a function that takes two numbers and an operator and then calls one of the above functions
+function operate(a, operator, b) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+        default:
+            result = 'Error'
+    }
+    return result;
+}
