9889e131766376504d69daf846e9a08e39ed7222
ZainF24 <165853011+ZainF24@users.noreply.github.com>
2024-08-08 18:11:07 -0500

    Created event listners for all buttons.

--- script.js
+++ script.js
@@ -37,3 +37,51 @@ function operate(a, operator, b) {
     return result;
 }
 // Event listeners for the buttons
+const button = document.getElementById("container");
+container.addEventListener("click", function(event) {
+    // Ensure clicked element is a button
+    if (event.target.tagName === "button") {
+        // Check to see if it's an operand
+        if (event.target.classList.contains("operand")) {
+
+        }
+        //Check to see if it's an equal
+        else if (event.target.classList.contains("equal")) {
+
+        }
+        // Check to see if it's 'zero'
+            else if (event.target.classList.contains("zero")) {
+
+        }
+            // Check to see if it's AC
+            else if (event.target.classList.contains("AC")) {
+
+        }
+            // Check to see if it's positive or negative sign
+            else if (event.target.classList.contains("sign")) {
+
+        }
+            // Check to see if it's percent
+            else if (event.target.classList.contains("percent")) {
+
+        }
+            // Check to see if it's a decimal
+            else if (event.target.classList.contains("decimal")) {
+
+        }
+        // Check to see if it's an operator
+        else if (event.target.classList.contains("operator")){
+            // Add
+            if (event.target.classList.contains("add")){
+
+            }
+            else if (event.target.classList.contains("subtract")){
+
+            }
+            else if (event.target.classList.contains("multiply")){}
+        }
+        else if (event.target.classList.contains("divide")){
+
+        }
+    }
+});
